03442afdacbeb4a6a013ea06b8edb6ccef72e628
Jacob Zimmerman <jzimmerman@collaborative.com>
2014-08-11 17:33:33 -0500

    Started documentation on control flow expressions

--- test/func/java/UnlessExpressionQuickTest.java
+++ test/func/java/UnlessExpressionQuickTest.java
@@ -1,13 +1,13 @@
 package func.java;
 
-import static func.java.controlflow.expressions.ExpressionHub.returns_;
+import static func.java.controlflow.expressions.ExpressionHub.return_;
 import java.util.Optional;
 
 public class UnlessExpressionQuickTest
 {
 	public static void main(String[] args)
 	{
-		Optional<String> output = returns_(() -> "output").unless_(true);
+		Optional<String> output = return_(() -> "output").unless_(true);
 		
 		System.out.println(output.orElse("no go"));
 	}
