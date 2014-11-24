32af281d79c8125f2b836ebd79046c1f4c728128
Jacob Zimmerman <jzimmerman@collaborative.com>
2014-11-24 15:18:06 -0600

    Thread-naming

--- README.md
+++ README.md
@@ -14,6 +14,7 @@ Items included in the library:
   + ExpressionHub and StatementHub - a single place to do a static import to begin using any of the provided control flow expressions or statements (func.java.controlflow.expressions.ExpressionHub and func.java.controlflow.statements.StatementHub)
 + Provider interface - a nice little helpful interface that provides a clean and easy way to create a Supplier, especially for testing, without a lambda expression (func.java.interfaces.Supply)
 + Lazy Instantiator - a class to make lazily (and thread-safely) instantiating objects easy (func.java.lazyinstantiator.LazilyInstantiate)
++ Thread Naming Runnables and Callables - pass a name and a Runnable/Callable into these classes' factory methods and get back a new Runnable/Callable that changes the name of the Thread they run in while they're being run. (func.java.concurrency.ThreadNamingRunnable && ThreadNamingCallable)
 
 Coming Soon:
 + Documentation for the custom control flow stuff
@@ -21,6 +22,7 @@ Coming Soon:
 + Collection wrapper, providing simple internal looping without Stream
 + "With" keyword - similar to "try with resources" but isn't a try-catch, and you can specify the closing method
 + Async Task - just creates a new thread and runs the given code - takes callbacks as well 
++ Python-like generators
 
 Looking Into:
 + "Until" loop - similar to Ruby's until loop
---
+++ src/func/java/concurrency/ThreadNamingCallable.java
@@ -0,0 +1,47 @@
+package func.java.concurrency;
+
+import java.util.concurrent.Callable;
+
+/**
+ * {@code ThreadNamingRunnable} is used to make {@code Callable}s that change the
+ * name of the thread they're in to make debugging the thread easier.
+ * <p>
+ * To do so, simply make a call to {@code ThreadNamingRunnable.namedThreadFrom()},
+ * passing in the name you want to give the thread and the callable code you want
+ * the thread to run.</p>
+ * Example (with static import of {@code namedThreadFrom}):
+ * <pre><code>executorService.submit(namedThreadFrom("my thread name", () -> "returned String");)
+ * </code></pre>
+ */
+public class ThreadNamingCallable<T> implements Callable<T>
+{
+	public static <T> ThreadNamingCallable<T> namedThreadFrom(String threadName, Callable<T> toCall)
+	{
+		return new ThreadNamingCallable<>(threadName, toCall);
+	}
+	
+	public T call() throws Exception
+	{
+		final Thread currThread = Thread.currentThread();
+		final String oldName = currThread.getName();
+		currThread.setName(threadName);
+		
+		try
+		{
+			return decoratedCallable.call();
+		}
+		finally
+		{
+			currThread.setName(oldName);
+		}
+	}
+	
+	private ThreadNamingCallable(String threadName, Callable<T> decoratedCallable)
+	{
+		this.threadName = threadName;
+		this.decoratedCallable = decoratedCallable;
+	}
+	
+	private final String threadName;
+	private final Callable<T> decoratedCallable;
+}
