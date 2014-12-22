e3cdfeadc49382e21a4e0be258061734539a6c03
Jacob Zimmerman <jzimmerman@collaborative.com>
2014-12-22 16:57:57 -0600

    TODO: consumer builder

---
+++ src/func/java/collections/ConsumerBuilder.java
@@ -0,0 +1,8 @@
+package func.java.collections;
+
+public abstract class ConsumerBuilder
+{
+	//TODO build this
+	//Its purpose is to serve as a fluent way to build Consumers for internal iterators
+	//Its api will seem like a whittled down version of Stream
+}
