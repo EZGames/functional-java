8e813856bdd2d83bed544db0b39286a49f7d4d82
Jacob Zimmerman <jzimmerman@collaborative.com>
2015-02-06 10:21:09 -0600

    Removed concurrency folder
    Added it to new repo: CustomThreads

--- src/func/java/concurrency/ThreadPriorityVerifier.java
+++
@@ -1,9 +0,0 @@
-package func.java.concurrency;
-
-public class ThreadPriorityVerifier
-{
-	public static boolean isLegal(int priority)
-	{
-		return priority < Thread.MAX_PRIORITY && priority > Thread.MIN_PRIORITY;
-	}
-}
